8d7e6193f1036ca4f4b805ff5f6a7634b8407e89
wtsd <warlockfx@gmail.com>
2025-09-30 18:04:24 -0400

    One-liner

--- examples/aws/one-time/s3_versioned_bucket/main.tf
+++ examples/aws/one-time/s3_versioned_bucket/main.tf
@@ -2,6 +2,7 @@ terraform {
   required_version = ">= 1.5.0"
   required_providers { aws = { source = "hashicorp/aws", version = "~> 5.0" } }
 }
+
 provider "aws" {}
 resource "aws_s3_bucket" "this" {
   bucket        = var.bucket_name
--- examples/aws/one-time/s3_versioned_bucket/variables.tf
+++ examples/aws/one-time/s3_versioned_bucket/variables.tf
@@ -1,3 +1,13 @@
-variable "bucket_name" { type = string }
-variable "force_destroy" { type = bool, default = false }
-variable "tags" { type = map(string), default = {} }
+variable "bucket_name" {
+  type = string 
+}
+
+variable "force_destroy" {
+  type    = bool
+  default = false
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
